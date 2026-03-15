8b3273eb2431005ce008af931df9d67c30bd9475
Dduong2195 <duong.dtt.2420@aptechlearning.edu.vn>
2026-03-15 14:06:01 +0700

    fix add nhieu anh thanh cong

---
+++ p3_backend/p3_backend/Models/DTO/OrderDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace p3_backend.DTOs
+{
+    public class OrderCreateDto
+    {
+        public int CustId { get; set; }
+        public string ShippingAddress { get; set; } = null!;
+        public string Status { get; set; } = "Pending";
+
+        // Danh sách các ảnh đi kèm đơn hàng này
+        public List<OrderDetailCreateDto> OrderDetails { get; set; } = new List<OrderDetailCreateDto>();
+    }
+}
---
+++ p3_backend/p3_backend/Models/DTO/OrderDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace p3_backend.DTOs
+{
+    public class OrderDetailCreateDto
+    {
+        public int PhotoId { get; set; }
+        public int SizeId { get; set; }
+        public int Quantity { get; set; }
+        public decimal PricePerCopy { get; set; }
+    }
+}
